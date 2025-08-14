d6e2fb68b2adcd633ce7beb28a9250d7bae3aeb1
Kyle Klus <kyle.klus@f-i.de>
2025-08-14 21:52:51 +0200

    FEAT: Added nice fav icon

--- rsbuild.config.mjs
+++ rsbuild.config.mjs
@@ -5,6 +5,7 @@ export default defineConfig({
 	plugins: [pluginReact()],
 	html: {
 		title: "Calendar Manager",
+		favicon: "./public/calendar-check.svg",
 	},
 	server: {
 		port: process.env.NODE_ENV === "development" ? 3000 : 8080,
