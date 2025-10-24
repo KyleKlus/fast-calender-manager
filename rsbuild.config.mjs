0468ee331511a0d0e2b802c5635e9f0f12c083b2
Kyle Klus <kyle.klus@f-i.de>
2025-10-24 14:54:18 +0200

    CHANGE: Prepped web app for desktop app conversion

--- rsbuild.config.mjs
+++ rsbuild.config.mjs
@@ -3,18 +3,23 @@ import { pluginReact } from "@rsbuild/plugin-react";
 
 export default defineConfig({
 	plugins: [pluginReact()],
+	source: {
+		entry: {
+			"fast-calender-manager": "./src/web/index.tsx",
+		},
+	},
 	html: {
 		title: "Calendar Manager",
 		favicon: "./public/calendar-check.svg",
 	},
 	server: {
 		port: process.env.NODE_ENV === "development" ? 3000 : 8080,
-		base: "/fast-calender-manager/",
+		base: "/",
 	},
 	output: {
 		cssModules: {
 			auto: true,
 		},
-		assetPrefix: "/fast-calender-manager/",
+		assetPrefix: "/",
 	},
 });
